c5cc3c2a97f800575cd8073e826ebc959e8fbeb2
jessicavipin <jessicavipin7@gmail.com>
2022-07-26 21:30:49 +0800

    added clear button

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import sys
+from tkinter import Label
 from PyQt5.QtWidgets import (QApplication, QWidget,
-QPushButton, QGridLayout)
+QPushButton, QGridLayout, QLabel)
+from PyQt5 import QtCore
 
 class Example(QWidget):
 
@@ -11,6 +13,11 @@ class Example(QWidget):
         self.setLayout(grid)
         self.button= []
         self.play="X"
+        self.count=0
+        widget = QWidget()
+        text= Label(widget)
+        text.setText("Checking")
+        grid.addLayout(text)
 
         names = [" "]*9
 
@@ -24,9 +31,11 @@ class Example(QWidget):
             
 
             button.clicked.connect(lambda _, kay=k: self.on_click(kay))
-            # print(k, fn())
             grid.addWidget(button, *position)
-        print(grid.itemAtPosition(0,0))
+        
+        clear = QPushButton("Clear",self)
+        grid.addWidget(clear,4,1)
+        clear.clicked.connect(restart)
         self.move(300, 150)
         self.setWindowTitle('PyQt window')  
         self.show()
@@ -37,6 +46,7 @@ class Example(QWidget):
     def on_click(self,k):
         print(k)
         if self.button[k].text()==" ":
+            self.count=self.count+1
             if self.play=="X" :
                 self.button[k].setText("O")
                 self.play="O"
@@ -61,17 +71,31 @@ class Example(QWidget):
                 self.end(self.play)
                 print(4)
                 return
+        if self.count==9:
+            self.end("Lost")
         return
 
     def end(self,winner):
+        
+
+
         for i in range(1,9):
             self.button[i].hide()
-        self.button[0].setText(winner +" wins!!!!")
-        #self.button[1].clicked.connect(self.restart())
+        if winner=="Lost":
+            self.button[0].setText("~~~   Nobody Wins   ~~~ ")
+        else:
+            self.button[0].setText(winner +" wins!!!!")
+    
+        
     
     #def restart(self):
 
         #os.execl(sys.executable, sys.executable, *sys.argv)
+def restart():
+        QtCore.QCoreApplication.quit()
+        status = QtCore.QProcess.startDetached(sys.executable, sys.argv)
+        print(status)
+
     
 
         
@@ -87,4 +111,7 @@ class Example(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
+    button = QPushButton("Restart")
+    button.clicked.connect(restart)
+
     sys.exit(app.exec_())  
